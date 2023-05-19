d88fa4f577885ff1bfebb99fa3ffa070f36d1d2e
jotabox <jotapaulo.box@gmail.com>
2023-05-19 18:08:58 -0300

    commit , animação pulo , movimento , correção do pulo
    correção do pulo , feito a animação de pulo , walk é idle , modificado o sistema de pulo para addForce ,, feito uma transição para ficar responsavel pelas animações

--- plataforma2d/Assets/Scripts/Player.cs
+++ plataforma2d/Assets/Scripts/Player.cs
@@ -10,8 +10,13 @@ public class Player : MonoBehaviour
     private float moveX;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    public Transform isGroundPosition;
+    public Transform spritePlayer;
+    //public Animator anim;
+    public float radius;
     private float moveY;
     private Rigidbody2D rigi;
+    public LayerMask layer;
 
     // Start is called before the first frame update
     void Start()
@@ -34,15 +39,35 @@ public class Player : MonoBehaviour
     {
         onMove();
         onJump();
+        //confirmedGrounded();
     }
 
     void onMove()
     {
+
+        Collider2D isGround = Physics2D.OverlapCircle(isGroundPosition.position, radius, layer);
+
+
         moveX = Input.GetAxisRaw("Horizontal");
         rigi.velocity = new Vector2(moveX * moveSpeed, rigi.velocity.y);
 
-        if (moveX > 0) { transform.eulerAngles = new Vector3(0, 0, 0); }
-        if (moveX < 0) { transform.eulerAngles = new Vector3(0, 180, 0); }
+        if (moveX > 0 && isGround)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+                spritePlayer.GetComponent<Animator>().SetInteger("transition", 1);
+        }
+        if (moveX < 0 && isGround) 
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+                spritePlayer.GetComponent<Animator>().SetInteger("transition", 1);
+        }
+
+        if (isGround && moveX == 0)
+        {
+            spritePlayer.GetComponent<Animator>().SetInteger("transition", 0);
+        }
+
+
         //TODO animação de caminhada
 
     }
@@ -50,10 +75,19 @@ public class Player : MonoBehaviour
 
     void onJump()
     {
-        if (Input.GetButtonDown("Jump"))
+        Collider2D isGround = Physics2D.OverlapCircle(isGroundPosition.position, radius, layer);
+        //if(isGround != null) { Debug.Log("tocou o chao"); }
+
+        if (Input.GetButtonDown("Jump") && isGround)
         {
-            rigi.velocity = new Vector2(rigi.velocity.x, jumpForce * Time.fixedDeltaTime );
-            //TODO , verificar se esta ocando no chão , animeção de pula
+            rigi.AddForce(Vector2.up * jumpForce , ForceMode2D.Impulse);
+                spritePlayer.GetComponent<Animator>().SetInteger("transition", 2);
         }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(isGroundPosition.position, radius);
     }
 }
